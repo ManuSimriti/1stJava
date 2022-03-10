00e286c957d594be385c6f6b366e8c18fb54eba1
Manpreet1011 <87238220+Manpreet1011@users.noreply.github.com>
2022-03-09 19:58:44 -0500

    Update Main1.java

--- src/com/company/Main1.java
+++ src/com/company/Main1.java
@@ -3,6 +3,6 @@ package com.company;
 public class Main1 {
     public static void main(String [] args) {
         System.out.println("Hello");
-        System.out.println("Hi");
+        System.out.println("Hi");//jdhasjhdsadhashd
     }
 }
